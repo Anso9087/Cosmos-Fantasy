601893260574ea86e1e66279875861849d58f45b
Ngai Yat Fung <anson6932@gmail.com>
2024-04-15 23:50:01 +0800

    pp and ss bug fixed, can return to minigame scene

--- Assets/Minigame/ss/Assets/Scripts/Snake.cs
+++ Assets/Minigame/ss/Assets/Scripts/Snake.cs
@@ -89,7 +89,8 @@ public class Snake : MonoBehaviour
 
         for (int i = 1; i < bodyParts.Count; i++) { Destroy(bodyParts[i].gameObject); } // start at 1 to not destroy head
 
-        if (dieIsRebody) { bodyParts.Clear(); bodyParts.Add(transform);} // no more body but add back head
+        if (dieIsRebody) { bodyParts.Clear(); bodyParts.Add(transform);}// no more body but add back head
+             // if no rebody = load 1 scene
 
         for (int i = 0; i < initialSize - 1; i++) { Grow(); } // -1 (head is in list)
 
@@ -102,7 +103,7 @@ public class Snake : MonoBehaviour
         else if (other.gameObject.CompareTag("Apple")) { Grow(); Grow(); } //bugs
         else if (other.gameObject.CompareTag("Poison")) { Grow(); poisonCam(Random.RandomRange(2,6)); }
         else if (other.gameObject.CompareTag("Obstacle")) { ResetState(); } // maybe I will updata new things
-        else if (other.gameObject.CompareTag("Wall")) {if (wallTP) { Traverse(other.transform); } else { ResetState(); }}}
+        else if (other.gameObject.CompareTag("Wall")) {if (wallTP) { Traverse(other.transform); } else {  SceneManager.LoadScene("Minigame1"); }}}
     private void Traverse(Transform wall)
     {   Vector3 position = transform.position;
         if (direction.x != 0f) { position.x = -wall.position.x + direction.x; } // can use Mathf.RoundToInt()
