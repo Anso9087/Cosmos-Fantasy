c99a6b514198cbbf927603daea599f531d67b14e
Ginkospace <143688097+Ginkospace@users.noreply.github.com>
2024-05-13 00:26:55 +0800

    Update Food.cs commit
    commit

--- Assets/Minigame/ss/Assets/Scripts/Food.cs
+++ Assets/Minigame/ss/Assets/Scripts/Food.cs
@@ -1,32 +1,30 @@
-﻿using UnityEditor;
+using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class Food : MonoBehaviour
 {
-    public static Food instance;
-    
+    public static Food instance; // get instance
+
     public Collider2D foodArea; // get food spawn area
     private Snake snake; // get small snake
 
-    private void Awake() { snake = FindObjectOfType<Snake>(); } // find
+    private void Awake() { snake = FindObjectOfType<Snake>(); } // find snake
     private void Start() { RandomizePosition(); } // random food spawn
 
     public void RandomizePosition()
     {   Bounds bounds = foodArea.bounds; // find food spawn bounds
 
-        // Pick a random position inside the bounds
-        // Round the values to ensure it aligns with the grid
+       
         float x = Random.Range(bounds.min.x, bounds.max.x); // can use Mathf.RoundToInt() to make it blockful
-        float y = Random.Range(bounds.min.y, bounds.max.y);
+        float y = Random.Range(bounds.min.y, bounds.max.y); // random pos inside the bounds
 
-        // Prevent the food from spawning on the snake
-        while (snake.Occupies((int) x, (int)y)) { 
+        while (snake.Occupies((int) x, (int)y)) { // if snake here // food can't spawn in the snake
             x++; if (x > bounds.max.x) { x = bounds.min.x; y++; if (y > bounds.max.y) { y = bounds.min.y; } }// can use Mathf.RoundToInt()
         }
-        transform.position = new Vector2(x, y);
+        transform.position = new Vector2(x, y); // pos
     }
 
     
-    private void OnTriggerEnter2D(Collider2D other){ RandomizePosition(); }
+    private void OnTriggerEnter2D(Collider2D other){ RandomizePosition(); } // randomized position
 }
